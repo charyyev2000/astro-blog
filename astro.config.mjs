fc7f087665dfaaa305f1f13efe9f4d5c23da225c
Myrat Charyyev <charyyev2110@gmail.com>
2024-06-28 01:54:32 -0500

    Added Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
